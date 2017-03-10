92ea544b1afab0e6b00d56938bf41b675a3f5ece
jroca72 <jroca72@gmail.com>
2017-03-10 16:14:03 +0100

    empezamos con el modelo de comunes

--- gladio/pugio/admin.py
+++ gladio/pugio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from pugio.models import provincia, poblacion, moneda, impuesto
 # Register your models here.
+
+admin.site.register(provincia)
+admin.site.register(poblacion)
+admin.site.register(moneda)
+admin.site.register(impuesto)
+
--- gladio/pugio/models.py
+++ gladio/pugio/models.py
@@ -1,5 +1,52 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+# COMUN
+
+# provncias -------------------------------------------------
+class provincia(models.Model):
+	numero = models.PositiveIntegerField(null=False, blank=False)
+	provincia = models.CharField(max_length=50)
+
+	class Meta:
+		verbose_name_plural = "Provincias"
+
+	def __unicode__(self):
+		return self.provincia
+
+# poblaciones -----------------------------------------------
+class poblacion(models.Model):
+   provincia = models.ForeignKey(provincia, default=None, null=True, blank=True)
+   poblacion = models.CharField(max_length=125)
+   
+   class Meta:
+		verbose_name_plural = "Poblaciones"
+
+   def __unicode__(self):
+	   return self.poblacion
+
+# monedas ---------------------------------------------------
+class moneda(models.Model):
+	moneda = models.CharField(max_length=20, null=False, blank=False)
+	
+	class Meta:
+	   verbose_name_plural = "Monedas"
+	
+	def __unicode__(self):
+		return self.moneda
+
+# impuestos --------------------------------------------------
+class impuesto(models.Model):
+   impuesto = models.CharField(max_length=25, null=False, blank=False)		   
+   porcentaje = models.DecimalField(max_digits=5, decimal_places=2)
+   
+   class Meta:
+      verbose_name_plural = "Impuestos"
+      
+   def __unicode__(self):
+      return self.impuesto
+
+         
