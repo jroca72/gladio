085d010fc8102caf9bd44d025cde812e0d4394e1
jroca72 <jroca72@gmail.com>
2017-03-10 17:06:56 +0100

    cambios/pruebas interfaz administrativa

--- gladio/pugio/admin.py
+++ gladio/pugio/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from pugio.models import provincia, poblacion, moneda, impuesto
 # Register your models here.
+class PoblacionAdmin(admin.ModelAdmin):
+   list_display = ('poblacion', 'provincia')
+   search_fields = ('poblacion', 'provincia__provincia')
+   list_filter = ('provincia__provincia',)
+   ordering = ('provincia__provincia', 'poblacion')
 
 admin.site.register(provincia)
-admin.site.register(poblacion)
+admin.site.register(poblacion, PoblacionAdmin)
 admin.site.register(moneda)
 admin.site.register(impuesto)
 
