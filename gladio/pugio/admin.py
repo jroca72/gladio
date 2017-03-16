8bfc6e10056e7604c14c587e3d4967a02e9b4db5
jroca72 <jroca72@gmail.com>
2017-03-16 17:04:43 +0100

    más models

--- gladio/pugio/admin.py
+++ gladio/pugio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from pugio.models import provincia, poblacion, moneda, impuesto, cliente
+from pugio.models import provincia, poblacion, moneda, impuesto, cliente, empresa
+from pugio.models import departamento, empleado
 # Register your models here.
 class PoblacionAdmin(admin.ModelAdmin):
    list_display = ('poblacion', 'provincia')
@@ -7,9 +8,19 @@ class PoblacionAdmin(admin.ModelAdmin):
    list_filter = ('provincia__provincia',)
    ordering = ('provincia__provincia', 'poblacion')
 
+class ClienteAdmin(admin.ModelAdmin):
+   list_display = ('apellidos', 'nombre', 'telefono', 'movil')
+   ordering = ('apellidos', 'nombre')
+
+class EmpleadoAdmin(admin.ModelAdmin):
+   list_display = ('empresa', 'apellidos', 'nombre', 'departamento')
+   
 admin.site.register(provincia)
 admin.site.register(poblacion, PoblacionAdmin)
 admin.site.register(moneda)
 admin.site.register(impuesto)
-admin.site.register(cliente)
+admin.site.register(cliente, ClienteAdmin)
+admin.site.register(empresa)
+admin.site.register(departamento)
+admin.site.register(empleado, EmpleadoAdmin)
 
--- gladio/pugio/models.py
+++ gladio/pugio/models.py
@@ -52,6 +52,7 @@ class impuesto(models.Model):
 # clientes ----------------------------------------------------
 class cliente(models.Model):
    nombre = models.CharField(max_length=100, null=False, blank=False)
+   apellidos = models.CharField(max_length=100, null=True, blank=True)
    direccion =models.CharField(max_length=100, null=False, blank=False)
    codigopostal = models.CharField(max_length=10, null=True, blank=True)
    poblacion = models.ForeignKey(poblacion, default=None, null=False, blank=False)
@@ -71,5 +72,62 @@ class cliente(models.Model):
       
    def __unicode__(self):
       return self.nombre
-            
+
+#empresas ------------------------------------------------------            
+class empresa(models.Model):
+   razon_social = models.CharField(max_length=100, null=False, blank=False)
+   cif = models.CharField(max_length=15, null=False, blank=False)
+   direccion_social = models.CharField(max_length=150, null=False, blank=False)
+   provincia_social = models.ForeignKey(provincia, default=None, null=False, blank=False)
+   poblacion_social = models.ForeignKey(poblacion, default=None, null=False, blank=False)
+   cp_social = models.CharField(max_length=10, null=True, blank=True)
+   pais = models.CharField(max_length=50, default='Espana', null=True, blank=True)
+   telefono_social = models.CharField(max_length=15, null=True, blank=True)
+   movil_social = models.CharField(max_length=15, null=True, blank=True)
+   fax_social = models.CharField(max_length=15, null=True, blank=True)
+   mail_social = models.EmailField(max_length=125, blank=True)
+   web = models.URLField(max_length=150, blank=True)
+   logotipo = models.ImageField(upload_to='imagenes', blank=True)
+   nombre_comercial = models.CharField(max_length=100, null=False, blank=False)
    
+   class Meta:
+      verbose_name_plural = "Empresas"
+      
+   def __unicode__(self):
+      return self.razon_social
+
+#departamento ----------------------------------------------------
+class departamento(models.Model):
+   departamento = models.CharField(max_length=75, null=False, blank=False)
+   
+   class Meta:
+      verbose_name_plural = "Departamentos"
+   
+   def __unicode__(self):
+      return self.departamento
+         
+#empleados -------------------------------------------------------
+class empleado(models.Model):
+   empresa = models.ForeignKey(empresa, default=None, null=False, blank=False)
+   nombre = models.CharField(max_length=75, null=False, blank=False)
+   apellidos = models.CharField(max_length=75, null=False, blank=False)
+   direccion = models.CharField(max_length=150, null=False, blank=False)
+   provincia = models.ForeignKey(provincia, default=None, null=False, blank=None)
+   poblacion = models.ForeignKey(poblacion, default=None, null=False, blank=None)
+   cp = models.CharField(max_length=10, null=True, blank=True)
+   pais = models.CharField(max_length=50, default='Espana', null=True, blank=True)
+   telefono = models.CharField(max_length=15, null=True, blank=True)
+   movil = models.CharField(max_length=15, null=True, blank=True)
+   mail = models.EmailField(max_length=125, blank=True)           
+   fotografia = models.ImageField(upload_to='imagenes', blank=True)         
+   nif = models.CharField(max_length=15, null=False, blank=False)   
+   nss = models.CharField(max_length=50, null=False, blank=False)   
+   departamento = models.ForeignKey(departamento, default=None, null=False, blank=None)
+   
+   class Meta:
+      verbose_name_plural = "Empleados"
+      
+   def __unicode__(self):
+      return self.nombre
+
+         
